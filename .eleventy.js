46f716611590a6e84ac5fb3da0fb96212b188a81
Zach Kehs <zach.kehs@gmail.com>
2025-12-21 15:02:23 -0800

    Migrate 11ty from 2.x -> 3.x
    I used the upgrade helper plugin: https://www.11ty.dev/docs/plugins/upgrade-help/
    
    Seemed to work well but my build command was not what they said, I just did the usual site serve command and it worked. okay.
    
    Leaving the ci tag off of this commit intentionally to hopefully kick off a deployment, since I think that's how this works. we will see.

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,8 @@
-module.exports = function(eleventyConfig) {
-
-    // Folders to copy to output folder
-    eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPassthroughCopy("CNAME");
-    return {
-        pathPrefix: "/website/"
-    }
+module.exports = function (eleventyConfig) {
+  // Folders to copy to output folder
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("CNAME");
+  return {
+    pathPrefix: "/website/",
+  };
 };
-
-
-
