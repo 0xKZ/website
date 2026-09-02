b2d43479d63afb06c6cc134884e9f56a19c05b86
Zach Kehs <zach.kehs@gmail.com>
2026-09-02 12:52:38 -0700

    add footnotes plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,7 @@ import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import footnotes from "eleventy-plugin-footnotes";
 
 import pluginFilters from "./_config/filters.js";
 
@@ -64,6 +65,9 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginNavigation);
   eleventyConfig.addPlugin(HtmlBasePlugin);
   eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+
+  // Footnotes: https://kittygiraudel.github.io/accessible-footnotes/eleventy/overview
+  eleventyConfig.addPlugin(footnotes);
 
   eleventyConfig.addPlugin(feedPlugin, {
     type: "atom", // or "rss", "json"
