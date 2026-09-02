8c23da56f72969d7f5768313128bc4977048293c
Zach Kehs <zach.kehs@gmail.com>
2026-09-02 14:59:01 -0700

    fix alphabetic sort being descending by default

--- _config/filters.js
+++ _config/filters.js
@@ -38,6 +38,6 @@ export default function(eleventyConfig) {
 	});
 
 	eleventyConfig.addFilter("sortAlphabetically", strings =>
-		(strings || []).sort((b, a) => b.localeCompare(a))
+		(strings || []).sort((a, b) => a.localeCompare(b))
 	);
 };
